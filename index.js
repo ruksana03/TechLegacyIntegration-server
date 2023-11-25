d6a043315d1fb10183274a8146f5f7eeefba027b
Ruksana <ruksana01official@gmail.com>
2023-11-26 03:01:58 +0600

    jwt updated

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+const port = process.env.PORT || 5000
+// const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+// const nodemailer = require('nodemailer')
+
+
+// middleware
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173','http://127.0.0.1:5173/'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const usersCollection = client.db('TLIdb').collection('users');
+
+
+        // token verification api start 
+        const verifyToken = async (req, res, next) => {
+            const token = req.cookies?.token
+            console.log(token)
+            if (!token) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.user = decoded
+                next()
+            })
+        }
+
+        //   token verification end 
+
+        // jwt api start 
+
+        // auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            console.log('new jwt:', user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+        //   jwt api end 
+
+        // Logout api start
+        app.get('/logout', async (req, res) => {
+            try {
+                res
+                    .clearCookie('token', {
+                        maxAge: 0,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    })
+                    .send({ success: true })
+                console.log('Logout successful')
+            } catch (err) {
+                res.status(500).send(err)
+            }
+        })
+        // logout api end 
+
+          // Save or modify user email, status in DB
+    app.put('/users/:email', async (req, res) => {
+        const email = req.params.email
+        const user = req.body
+        const query = { email: email }
+        const options = { upsert: true }
+        const isExist = await usersCollection.findOne(query)
+        console.log('User found?----->', isExist)
+        if (isExist) return res.send(isExist)
+        const result = await usersCollection.updateOne(
+          query,
+          {
+            $set: { ...user, timestamp: Date.now() },
+          },
+          options
+        )
+        res.send(result)
+      })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Tech server is Running')
+  })
+  
+  app.listen(port, () => {
+    console.log(`server running on port: ${port}`);
+  })
